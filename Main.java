da66d6d6f916c92c2ecc539b3df69d4a3d03edc5
Andrew Wang <andrew910730@gmail.com>
2019-04-21 11:30:45 +1000

    Added sleep

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String args[]) {
-
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
